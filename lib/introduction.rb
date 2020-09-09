4cec868fb5f96a5ee48c1bc91c879d176c288929
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-09 20:47:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,10 +2,6 @@
 
 def introduction(name)
 puts "Hi, my name is #{name}."
-end
-
-# lib/introduction.rb 
-
 def introduction_with_language(Dan, Ember)
   puts "Hi, my name is #{name} I am learning to program in #{language}."
 end
